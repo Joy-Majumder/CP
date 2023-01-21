17b46c2e85782f3c58c077512cf7ead731a024b9
Joy Majumdar <49155965+Joy-Majumder@users.noreply.github.com>
2023-01-21 20:01:23 +0600

    problem

---
+++ CodeForce/P-1772A.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+	int t;
+	scanf("%d\n", &t);
+	while (t--)
+	{
+		char a[5];
+		scanf("%s", &a);
+		printf("%d\n", a[0] + a[2] - 96);
+	}
+	return 0;
+}
